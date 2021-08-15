7e1dc4059535027d434543225008722deb507246
kykil <kilkiyoung@gmail.com>
2021-08-15 14:02:58 +0900

    [Chore] Add revalidate in swr

--- pages/LogIn/index.tsx
+++ pages/LogIn/index.tsx
@@ -7,7 +7,7 @@ import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
 
 const LogIn = () => {
-  const { data, error } = useSWR('/api/users', fetcher);
+  const { data, error, revalidate } = useSWR('/api/users', fetcher);
   const [logInError, setLogInError] = useState(false);
   const [email, onChangeEmail] = useInput('');
   const [password, onChangePassword] = useInput('');
@@ -19,7 +19,9 @@ const LogIn = () => {
         .post('/api/users/login', { email, password }, {
           withCredentials: true,
         })
-        .then(() => {})
+        .then(() => {
+          revalidate();
+        })
         .catch((error) => {
           setLogInError(error.response?.data?.statusCode === 401);
         });
