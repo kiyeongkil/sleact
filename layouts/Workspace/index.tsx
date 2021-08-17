4405d900dc629c037967c443842d8abeb39e36e0
kiyeongkil <kilkiyoung@gmail.com>
2021-08-17 21:56:08 +0900

    [Feat] Add gravatar
    - gravatar : 랜덤으로 이미지 생성 e.g)github
    - Workspace
    	jsx 생성

--- layouts/Workspace/index.tsx
+++ layouts/Workspace/index.tsx
@@ -3,6 +3,18 @@ import fetcher from '@utils/fetcher';
 import useSWR from 'swr';
 import axios from 'axios';
 import { Redirect } from 'react-router';
+import {
+  Channels,
+  Chats,
+  Header,
+  MenuScroll,
+  ProfileImg,
+  RightMenu,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from '@layouts/Workspace/styles';
+import gravatar from 'gravatar';
 
 const Workspace: FC = ({ children }) => {
   const { data, error, revalidate, mutate } = useSWR('/api/users', fetcher, {
@@ -25,7 +37,22 @@ const Workspace: FC = ({ children }) => {
 
   return (
     <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          </span>
+        </RightMenu>
+      </Header>
       <button onClick={onLogout}>로그아웃</button>
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>sleact</WorkspaceName>
+          <MenuScroll>menu scroll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
       {children}
     </div>
   );
