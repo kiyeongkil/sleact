0b55b6a8051207103cd5fe39ec77650a9095ea45
kiyeongkil <kilkiyoung@gmail.com>
2021-08-24 15:36:49 +0900

    [Chore] Chore DM page
    - DM화면 변경

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -1,7 +1,29 @@
+import { Container, Header } from '@pages/DirectMessage/styles';
 import React from 'react';
+import gravatar from 'gravatar';
+import fetcher from '@utils/fetcher';
+import useSWR from 'swr';
+import { useParams } from 'react-router';
 
 const DirectMessage = () => {
-  return <div>DM!</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR(`/api/users`, fetcher);
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      {/* <ChatList /> */}
+      {/* <ChatBox /> */}
+    </Container>
+  );
 };
 
 export default DirectMessage;
