75baa2b12d64315aca61404e6a1afd33cd451c85
kiyeongkil <kilkiyoung@gmail.com>
2021-08-25 14:56:37 +0900

    [Feat] Add ChatList
    - 채팅리스트를 ChatBox 위에 끼워넣기

--- pages/DirectMessage/index.tsx
+++ pages/DirectMessage/index.tsx
@@ -5,6 +5,7 @@ import fetcher from '@utils/fetcher';
 import useSWR from 'swr';
 import { useParams } from 'react-router';
 import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
 
 const DirectMessage = () => {
   const { workspace, id } = useParams<{ workspace: string; id: string }>();
@@ -21,7 +22,7 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      {/* <ChatList /> */}
+      <ChatList />
       <ChatBox chat="" />
     </Container>
   );
