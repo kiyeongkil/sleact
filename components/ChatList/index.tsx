0e6c04c3b9522c81df55ad8372cbc7a132073435
kiyeongkil <kilkiyoung@gmail.com>
2021-08-26 17:14:39 +0900

    [Feat] Add DM chat
    - DM 채팅 구현

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useCallback, useRef, VFC } from 'react';
 import { ChatZone, Section } from '@components/ChatList/styles';
+import { IDM } from '@typings/db';
+import Chat from '@components/Chat';
+import { Scrollbars } from 'react-custom-scrollbars';
 
-const ChatList = () => {
+interface Props {
+  chatData?: IDM[];
+}
+const ChatList: VFC<Props> = ({ chatData }) => {
   return (
     <ChatZone>
-      <Section>section</Section>
+      <Scrollbars autoHide>
+        {chatData?.map((chat) => (
+          <Chat key={chat.id} data={chat} />
+        ))}
+      </Scrollbars>
     </ChatZone>
   );
 };
