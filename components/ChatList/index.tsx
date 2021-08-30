8a2b3663569e864feb93daa2fee8f7cb1193a2e4
kiyeongkil <kilkiyoung@gmail.com>
2021-08-30 22:09:45 +0900

    [Feat] Add Section in chats
    - 채팅 날짜별로 묶어주기

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -1,19 +1,28 @@
 import React, { useCallback, useRef, VFC } from 'react';
-import { ChatZone, Section } from '@components/ChatList/styles';
+import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
 import { IDM } from '@typings/db';
 import Chat from '@components/Chat';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
-  chatData?: IDM[];
-}
-const ChatList: VFC<Props> = ({ chatData }) => {
+  chatSections: { [key: string]: IDM[] };
+} //커스텀 스크롤바 추가 필요
+const ChatList: VFC<Props> = ({ chatSections }) => {
   return (
     <ChatZone>
       <Scrollbars autoHide>
-        {chatData?.map((chat) => (
-          <Chat key={chat.id} data={chat} />
-        ))}
+        {Object.entries(chatSections).map(([date, chats]) => {
+          return (
+            <Section className={`section-${date}`} key={date}>
+              <StickyHeader>
+                <button>{date}</button>
+              </StickyHeader>
+              {chats.map((chat) => (
+                <Chat key={chat.id} data={chat} />
+              ))}
+            </Section>
+          );
+        })}
       </Scrollbars>
     </ChatZone>
   );
